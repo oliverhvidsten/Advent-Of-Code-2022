f301ca9749d498226b4bf9005d6ec4e5424d8cf2
Oliver Hvidsten <oliverhvidsten@Olivers-MacBook-Pro.local>
2022-12-21 01:03:35 -0500

    added function to return unformatted input text

--- utils.py
+++ utils.py
@@ -3,3 +3,9 @@ def read_textfile(filename):
 	with open(filename) as f:
 		lines = [line.strip() for line in f.readlines()]
 		return lines
+
+def raw_textfile(filename):
+	with open(filename) as f:
+		lines = [line.strip() for line in f.readlines()]
+		return lines
+
