dd8bcaf7f59c938db60bb0d43a44eca4df481560
Oliver Hvidsten <oliverhvidsten@Olivers-MacBook-Pro.local>
2022-12-21 14:27:37 -0500

    added Directory Class for day 7

--- utils.py
+++ utils.py
@@ -9,3 +9,29 @@ def raw_textfile(filename):
 		lines = [line.strip() for line in f.readlines()]
 		return lines
 
+
+class Directory:
+	def __init__(self, name, parent):
+		self.name = name
+		self.parent = parent
+		self.content = {}
+		self.size = 0
+	
+	def addFile(self, name, size):
+		self.content[name] = size
+
+	def addDirectory(self, name, parent):
+		self.content[name] = Directory(name, parent)
+
+	def calculateSize(self):
+		size = 0
+		for value in self.content.values():
+			if isinstance(value, int):
+				size += value
+			else:
+				size += value.calculateSize()
+		self.size = size
+		return size
+
+
+
