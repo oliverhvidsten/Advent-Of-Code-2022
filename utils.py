c56e8162c42effb826a05e7ef5ba759cd9efb3cc
Oliver Hvidsten <oliverhvidsten@Olivers-MacBook-Pro.local>
2022-12-21 22:44:11 -0500

    updated raw_textfile() function

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ def read_textfile(filename):
 
 def raw_textfile(filename):
 	with open(filename) as f:
-		lines = [line.strip() for line in f.readlines()]
+		lines = [line for line in f.readlines()]
 		return lines
 
 
